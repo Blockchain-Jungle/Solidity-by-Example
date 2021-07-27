885d964b1fcf2d32059567674d5c873d3a0b178f
Saing Sab <saing.sab@gmail.com>
2021-07-27 17:45:29 +0700

    saing.sab@gmail.com

---
+++ contracts/03Variables.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+// Variables
+// There are 3 types of variables in Solidity
+
+// local
+//  - declared inside a function
+//  - not stored on the blockchain
+// state
+//  - declared outside a function
+//  - stored on the blockchain
+// global (provides information about the blockchain)
+
+contract Variables {
+    // State variables are stored on the blockchain.
+    string public text = "Hello";
+    uint256 public num = 123;
+
+    function doSomething() public {
+        //Local variables are not saved to the blockchain.
+        uint256 i = 456;
+
+        // Here are some global variables
+        uint256 timestamp = block.timestamp;
+        address sender = msg.sender;
+    }
+}
