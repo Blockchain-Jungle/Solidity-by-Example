93c2e4008d2004f75bb9173cf99e013d1627593c
Saing Sab <saing.sab@gmail.com>
2021-07-29 15:07:22 +0700

    add Enum

---
+++ contracts/11Enum.sol
@@ -0,0 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+/*
+Solidity supports enumerables and they are useful to model choice and keep track of state.
+
+Enums can be declared ouside of a contract.
+*/
+contract Enum {
+    // Enum representing shipping status
+    enum Status {
+        Pending,
+        Shipped,
+        Accepted,
+        Rejected,
+        Caceled
+    }
+
+    // Default value is the first element listed in
+    // definition of the type, in this case "Pending"
+    Status public status;
+
+    // Returns uint
+    // Pending  - 0
+    // Shipped  - 1
+    // Accepted - 2
+    // Rejected - 3
+    // Canceled - 4
+
+    function get() public view returns (Status) {
+        return status;
+    }
+
+    // Update status by passing uint into input
+    function set(Status _status) public {
+        status = _status;
+    }
+
+    // You can update to a specific enum like this
+    function cancel() public {
+        status = Status.Caceled;
+    }
+
+    // delete resets the enum to it's first value, 0
+    function reset() public {
+        delete status;
+    }
+}
+
+/* Enum can be imported from file too 
+
+For Example the Enum from file that contain as bellow
+---------------------------------------------------
+pragma solidity ^0.8.6;
+
+import "./EnumDeclaration.sol";
+
+contract Enum {
+    Status public status;
+}
+--------------------------------------------------
+*/
