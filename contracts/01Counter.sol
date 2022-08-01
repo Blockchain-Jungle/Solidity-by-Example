7785d6f9ab6066ec53d71a10c4a79a2f7e518c5a
Saing Sab (Ayoung) <saing.sab@gmail.com>
2022-08-01 15:08:54 +0700

    Update 01Counter.sol

--- contracts/01Counter.sol
+++ contracts/01Counter.sol
@@ -1,21 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.6;
+pragma solidity ^0.8.13;
 
-// This simple contract is for increment and decrement.
 contract Counter {
-    uint256 public count;
+    uint public count;
 
-    // Function to get the current count
-    function get() public view returns (uint256) {
-        return count;
+    function get() public view returns (uint) {
+       return count; 
     }
 
-    // Function to set the increament on counter
     function inc() public {
         count += 1;
     }
 
-    // Function to decrement count
     function dec() public {
         count -= 1;
     }
